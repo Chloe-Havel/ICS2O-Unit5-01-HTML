ee8edb328b5637e7962def6c388abde1456ec012
Chloe Havel <113055595+Chloe-Havel@users.noreply.github.com>
2022-11-15 14:25:41 -0500

    added a lot

--- js/script.js
+++ js/script.js
@@ -4,20 +4,29 @@
 // Created on: Sep 2020
 // This file contains the JS functions for index.html
 
-"use strict"
-
 /**
  * Check servie worker.
  */
-if (navigator.serviceWorker) {
-  navigator.serviceWorker.register("/ICS2O-PWA-Test-1/sw.js", {
-    scope: "/ICS2O-PWA-Test-1/",
-  })
+  if (navigator.serviceWorker) {
+  navigator.serviceWorker.register(
+    "/ICS2O-Unit5-01-HTML-Slider-Example/sw.js",
+    {
+      scope: "/ICS2O-Unit5-01-HTML-Slider-Example/",
+    }
+  )
 }
 
 /**
- * This function displays an alert.
+ * This function updates the slider value.
+ */
+function updateSliderValue(valueFromSlider) {
+  document.getElementById("slider-value").innerHTML = valueFromSlider
+}
+
+/**
+ * This function displays the slider value.
  */
 function myButtonClicked() {
-  document.getElementById("hello-world").innerHTML = "<p>Hello, World!</p>"
+  document.getElementById("hello-world").innerHTML =
+    "<p>Value is: " + slider.value + "</p>"
 }
